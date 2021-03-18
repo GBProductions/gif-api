f6a8756cec94a5ac0ba24c3594ea296e768cfc61
Garrett Brown <garrettpaulbrown@gmail.com>
2021-03-17 20:55:15 -0700

    Add files, fix typos.

--- src/main.js
+++ src/main.js
@@ -1,38 +1,37 @@
-// import $, { data } from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
-$(document).ready(function() {
-  $('#famousName').click(function() {
-    let input = $('#name').val();
-    $('#name').val("");
+$('#submitButton').click(function(event) {
+  event.preventDefault();
+  let input = $('form#famousName #name').val();
+  $('#name').val("");
 
-    let promise = new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${input}&limit=5&offset=0&rating=g&lang=en`
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(request.response);
-        }
+  console.log(input);
+
+  let promise = new Promise(function(resolve, reject) {
+    let request = new XMLHttpRequest();
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${input}&limit=5&offset=0&rating=g&lang=en`;
+    request.onload = function() {
+      if (this.status === 200) {
+        resolve(request.response);
+      } else {
+        reject(request.response);
       }
-      request.open("GET", url, true);
-      request.send();
-    });
+    };
+    request.open("GET", url, true);
+    request.send();
+  });
 
-    data[0].images.original.url
-
-    promise.then(function(response) {
-      const body = JSON.parse(response);
-      $('.showName).text(`A Gif for ${input}`);
-      $('.showGif').append(``);
-      $('.showErrors').text("");
-    }, function(error) {
-      $('.showErrors').text(`There was an error processing your request: ${error}`);
-      $('.showName').text("");
-      $('.showGif').text("");
-    });
+  promise.then(function(response) {
+    const ipaResponse = JSON.parse(response);
+    $('.showName').text(`A Gif for ${input}`);
+    $('#showGif').append(`<img src="${ipaResponse.data[0].images.original.url}">`);
+    $('.showErrors').text("");
+  }, function(error) {
+    $('.showErrors').text(`There was an error processing your request: ${error}`);
+    $('.showName').text("");
+    $('#showGif').text("");
   });
 });
